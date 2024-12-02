04b63521417e0f16d674893349645aa354c426b4
coolshiva123 <coolshiva123@yahoo.com>
2024-12-02 22:01:51 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,9 @@
+variable instype {
+    default = "t2.micro"
+}
+variable ami2use {
+    default = "ami-0427090fd1714168b"
+}
 resource "aws_instance" "my_ec2_instance" {
     ami = var.ami2use
     instance_type = var.instype 
